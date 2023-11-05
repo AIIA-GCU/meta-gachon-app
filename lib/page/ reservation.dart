16b3868ceef080842b21eda2eab8320de25573bf
ChooHoSeong <49583624+ChooHoSeong@users.noreply.github.com>
2023-11-05 21:09:38 +0900

    create reservation.dart
    "예약하기"페이지 작성을 위한 파일

---
+++ lib/page/ reservation.dart
@@ -0,0 +1,17 @@
+///추호성이꺼
+
+import 'package:flutter/material.dart';
+import 'package:mata_gachon/config/variable.dart';
+
+class Reservation extends StatelessWidget {
+  const Reservation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
